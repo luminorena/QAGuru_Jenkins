e27697021780686742da96590ff914f7541c47bb
luminorena <bakhuta.oa@gmail.com>
2022-10-31 00:21:27 +0300

    #19 Refactoring: video added

--- src/test/java/helpers/TestBase.java
+++ src/test/java/helpers/TestBase.java
@@ -5,32 +5,28 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.net.MalformedURLException;
 import java.util.Map;
 
 public class TestBase {
     @BeforeAll
-    static void setUp() throws MalformedURLException {
+    static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        Configuration.browserCapabilities = getRemoteDriver();
-    }
-
-    private static MutableCapabilities getRemoteDriver() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("browserName", "chrome");
         capabilities.setCapability("browserVersion", "100.0");
         capabilities.setCapability("selenoid:options",
                 Map.of("enableVNC", true, "enableVideo", true));
-        return capabilities;
+        Configuration.browserCapabilities = capabilities;
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
     }
+
 
     @AfterEach
     void addAttachments() {
